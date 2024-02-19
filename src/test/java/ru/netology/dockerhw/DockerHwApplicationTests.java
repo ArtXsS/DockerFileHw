c9f37190420a4b0bab23a7d70a1541c521430590
ArtXs <arti06tube@gmail.com>
2024-02-19 17:38:07 +0400

    Initial commit

---
+++ src/test/java/ru/netology/dockerhw/DockerHwApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.dockerhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
